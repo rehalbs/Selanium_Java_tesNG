a31e1289ed8566eee6e3016cc4c4c6015e124106
BarryNZ <70421819+rehalbs@users.noreply.github.com>
2024-09-10 19:16:45 +1200

    First Commit

---
+++ Introduction/src/SelIntroduction.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Chrome browser
+		WebDriver Cdriver = new ChromeDriver();
+		//driver.get(google.com);
+		Cdriver.get("https://rahulshettyacademy.com");
+		System.out.println(Cdriver.getTitle());
+		System.out.println(Cdriver.getCurrentUrl());
+		Cdriver.quit();
+		
+		WebDriver FDriver = new FirefoxDriver();
+		FDriver.get("https://rahulshettyacademy.com");
+		System.out.println(FDriver.getTitle());
+		System.out.println(FDriver.getCurrentUrl());
+		FDriver.quit();
+	}
+}
